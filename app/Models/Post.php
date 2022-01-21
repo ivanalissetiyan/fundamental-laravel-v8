85b979c2b4c3b2b742f3b5d6b0cbbeeb1f3529c3
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-01-21 11:32:01 +0700

    post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // arti dari fillable yaitu hanya dari ketiga hal tsb yang diperbolehkan diisi
+    // protected $fillable = ['title', 'excerpt', 'body'];
+
+    // arti dari guarded yaitu hanya id aja yg tidak di perbolehkan di isi
+    protected $guarded = ['id'];
 }
--- database/migrations/2022_01_19_110211_create_posts_table.php
+++ database/migrations/2022_01_19_110211_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('tesss');
+            $table->string('slug')->unique();
             $table->text('excerpt');
             $table->text('body');
             $table->timestamp('published_at')->nullable();

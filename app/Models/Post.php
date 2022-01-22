e24abb3a2c5b01e2a97a9e3d84038a1198680fe7
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-01-22 18:27:44 +0700

    membuat halaman author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,4 +19,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,8 @@
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <p>By. Ivan Alissetiyan <a href="/categories/{{ $post->category->slug }}"
+            <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> <a
+                    href="/categories/{{ $post->category->slug }}"
                     class="text-decoration-none">{{ $post->category->name }}</a>
             </p>
 

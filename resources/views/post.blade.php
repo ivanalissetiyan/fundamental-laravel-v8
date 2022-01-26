f983b957b16fc17a77579caeb05c74abd0a0cac7
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-01-26 15:00:28 +0700

    redesign UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,14 +2,25 @@
 
 @section('container')
 
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h1 class="mb-3">{{ $post->title }}</h1>
+                <p>By: <a href="/authors/{{ $post->author->username }}"
+                        class="text-decoration-none">{{ $post->author->name }}</a> in
+                    <a href="/categories/{{ $post->Category->slug }}"
+                        class="text-decoration-none">{{ $post->Category->name }}</a>
+                </p>
+                <img src="https://source.unsplash.com/1200x400?{{ $post->Category->name }}"
+                    alt="https://source.unsplash.com/1200x400?{{ $post->Category->name }}" class="img-fluid">
 
-    <h1 class="mb-5">{{ $post->title }}</h1>
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
 
-    <p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in
-        <a href="/categories/{{ $post->Category->slug }}" class="text-decoration-none">{{ $post->Category->name }}</a>
-    </p>
 
-    {!! $post->body !!}
-
-    <a class="d-block mt-3" href="/posts">Bact To Post</a>
+                <a class="d-block mt-3" href="/posts">Bact To Post</a>
+            </div>
+        </div>
+    </div>
 @endsection

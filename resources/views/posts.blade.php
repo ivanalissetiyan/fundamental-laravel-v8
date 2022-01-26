04a7badb741a36e913ac454ab336f5677bac5854
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-01-26 13:27:45 +0700

    membuat card untuk post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,45 +3,55 @@
 @section('container')
     <h1 class="mb-5">{{ $title }}</h1>
 
-    @if($posts->count())
-    <div class="card mb-3">
-        <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->Category->name }}" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
-            <p>
-             <small class="text-muted"> 
-                By. <a href="/authors/{{ $posts[0]->author->username }}"
-                    class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
-                    href="/categories/{{ $posts[0]->category->slug }}"
-                    class="text-decoration-none">{{ $posts[0]->category->name }}</a> Last updated {{ $posts[0]->created_at->diffForHumans() }}  mins ago
-            </small>
-            </p>
+    @if ($posts->count())
+        <div class="card mb-3">
+            <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->Category->name }}" class="card-img-top"
+                alt="https://source.unsplash.com/1200x400?{{ $posts[0]->Category->name }}">
+            <div class="card-body text-center">
+                <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
+                        class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+                <p>
+                    <small class="text-muted">
+                        By. <a href="/authors/{{ $posts[0]->author->username }}"
+                            class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
+                            href="/categories/{{ $posts[0]->category->slug }}"
+                            class="text-decoration-none">{{ $posts[0]->category->name }}</a> Last updated
+                        {{ $posts[0]->created_at->diffForHumans() }} mins ago
+                    </small>
+                </p>
 
-            <p class="card-text">{{ $posts[0]->excerpt }}</p>
-             <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read More...</a>
+                <p class="card-text">{{ $posts[0]->excerpt }}</p>
+                <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read More...</a>
 
-            
+
+            </div>
         </div>
-    </div>
     @else
         <p class="text-center fs-4">No Post Found</p>
     @endif
 
-    @foreach ($posts as $post)
-
-        <article class="mb-5 border-bottom pb-4">
-            <h2>
-                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
-            </h2>
-            <p>By. <a href="/authors/{{ $post->author->username }}"
-                    class="text-decoration-none">{{ $post->author->name }}</a> <a
-                    href="/categories/{{ $post->category->slug }}"
-                    class="text-decoration-none">{{ $post->category->name }}</a>
-            </p>
-
-            <p>{{ $post->excerpt }}</p>
-            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More...</a>
-        </article>
-    @endforeach
-
+    <div class="container">
+        <div class="row">
+            @foreach ($posts->skip(0) as $post)
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img src="https://source.unsplash.com/500x400?{{ $post->Category->name }}" class="card-img-top"
+                            alt="{{ $post->category->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p>
+                                <small class="text-muted">
+                                    By. <a href="/authors/{{ $post->author->username }}"
+                                        class="text-decoration-none">{{ $post->author->name }}</a>Last updated
+                                    {{ $post->created_at->diffForHumans() }} mins ago
+                                </small>
+                            </p>
+                            <p class="card-text">{{ $post->excerpt }}</p>
+                            <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
